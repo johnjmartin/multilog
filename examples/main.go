9a4064649f046959013eea924f5aef06378dcf9d
John Martin <johnjosephmartin@icloud.com>
2021-01-18 22:22:48 -0500

    improve example

--- examples/main.go
+++ examples/main.go
@@ -12,14 +12,27 @@ func main() {
 	mtl, err := multilog.NewMultiLog(map[string]string{
 		"server1":         "server1.log",
 		"database server": "db_server.log",
-		"server2":         "server2.log",
 	})
 	if err != nil {
 		fmt.Printf("Error initializing MultiLog %s, exiting", err)
 		return
 	}
 
-	t, _ := time.Parse(time.RFC3339, "2020-01-02T00:00:00Z")
+	t, _ := time.Parse(time.RFC3339, "2020-02-28T05:20:55Z")
+	fmt.Printf("query 1:\nQuery(2020-02-28T05:20:55Z, 10, []string{\"server1\", \"database server\"}, multilog.WARN)\n")
 	res, _ := mtl.Query(t, 10, []string{"server1", "database server"}, multilog.WARN)
-	fmt.Println(res)
+
+	fmt.Printf("result:\n%s\n", res)
+
+	t, _ = time.Parse(time.RFC3339, "2020-02-28T05:20:17Z")
+	fmt.Printf("query 2:\nQuery(2020-02-28T05:20:17Z, 5, []string{\"server1\", \"database server\"}, multilog.INFO)\n")
+	res, _ = mtl.Query(t, 5, []string{"server1", "database server"}, multilog.INFO)
+
+	fmt.Printf("result:\n%s\n", res)
+
+	t, _ = time.Parse(time.RFC3339, "2020-02-28T05:20:57Z")
+	fmt.Printf("query 2:\nQuery(2020-02-28T05:20:57Z, 3, []string{\"server1\", \"database server\"}, multilog.WARN)\n")
+	res, _ = mtl.Query(t, 3, []string{"server1", "database server"}, multilog.WARN)
+
+	fmt.Printf("result:\n%s\n", res)
 }
